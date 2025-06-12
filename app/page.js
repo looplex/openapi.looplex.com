3015b7acbad1b6208dd9a181e4e55f054d2a2b5e
Fabio Pereira dos Santos <fabio.santos@looplex.com.br>
2025-06-11 22:01:02 -0300

    docs: updating docs

--- app/page.js
+++ app/page.js
@@ -43,24 +43,32 @@ export default function Home() {
         </Col>
 
         <Col span={10} style={{ marginTop: 30 }}>
+
           <Search
             placeholder="Pesquise na Looplex OpenAPI"
             onSearch={() => { }}
-            suffix={
-              <AudioOutlined
-                style={{
-                  fontSize: 16,
-                  color: '#1677ff',
-                }}
-              />
-            }
+            suffix={<AudioOutlined style={{ fontSize: 16, color: '#1677ff' }} />}
           />
+
         </Col>
 
-        <Col span={5} style={{ marginLeft: 120, marginTop: 30 }}>
-          <FacebookOutlined style={{ fontSize: 26, color: '#1677FF' }} />
-          <LinkedinOutlined style={{ fontSize: 26, color: '#1677FF', marginLeft: 30 }} />
-          <InstagramOutlined style={{ fontSize: 26, color: '#1677FF', marginLeft: 30 }} />
+        <Col span={5} style={{ marginTop: 30 }} offset={3}>
+
+          <FacebookOutlined
+            onClick={() => { }}
+            style={{ fontSize: 26, color: '#1677FF' }}
+          />
+
+          <LinkedinOutlined
+            onClick={() => { }}
+            style={{ fontSize: 26, color: '#1677FF', marginLeft: 30 }}
+          />
+
+          <InstagramOutlined
+            onClick={() => { }}
+            style={{ fontSize: 26, color: '#1677FF', marginLeft: 30 }}
+          />
+
         </Col>
 
       </Row>
@@ -72,9 +80,11 @@ export default function Home() {
           <Row>
 
             <Col span={16}>
+
               <Text style={{ fontSize: 36, fontWeight: 'bold', marginTop: 50 }}>
                 Documentação - APIs Looplex
               </Text>
+
             </Col>
 
           </Row>
@@ -82,14 +92,18 @@ export default function Home() {
           <Row>
 
             <Col span={16}>
+
               <div style={{ fontSize: 18, marginTop: 30 }}>
                 Seja bem-vindo à página oficial de <b>documentação das APIs da Looplex.</b>
               </div>
+
               <br />
+
               <div style={{ fontSize: 18 }}>
                 Aqui você encontrará informações detalhadas, guias e exemplos para auxiliar na
                 integração eficiente dos nossos serviços.
               </div>
+
             </Col>
 
           </Row>
@@ -97,12 +111,14 @@ export default function Home() {
         </Col>
 
         <Col span={7}>
+
           <Image
             src={'/api_image.png'}
             width={500}
             height={350}
             alt='Looplex'
           />
+
         </Col>
 
       </Row>
