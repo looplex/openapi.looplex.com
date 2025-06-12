91814a22d290cd49262a7fb837278c14df88c25c
Fabio Pereira dos Santos <fabio.santos@looplex.com.br>
2025-06-11 21:53:31 -0300

    docs: updating docs

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 'use client'
 
 import React from 'react'
-import { Row, Col, Typography, Card, Input, Divider } from "antd"
+import { Row, Col, Typography, Card, Input } from "antd"
 import Image from 'next/image'
 
-import { 
-  AudioOutlined, 
-  FacebookOutlined, 
-  LinkedinOutlined, 
+import {
+  AudioOutlined,
+  FacebookOutlined,
+  LinkedinOutlined,
   InstagramOutlined,
   ApiOutlined
 } from '@ant-design/icons'
@@ -15,95 +15,13 @@ import {
 const { Search } = Input;
 const { Text } = Typography
 
-const apis = [
-  {
-    key: 'actions',
-    label: 'Actions',
-    children:
-      <>
-        <Card
-          title="Endpoints do Actions"
-          actions={[]}
-        >
-          <a href="/v1/actions" target="_blank">Actions v1</a>
-        </Card>
-      </>
-  },
-  {
-    key: 'assembler',
-    label: 'Assembler',
-    children:
-      <>
-        <Card
-          title="Endpoints do Assembler"
-          actions={[]}
-        >
-          <a href="/v1/assembler" target="_blank">Assembler v1</a>
-        </Card>
-      </>
-  },
-  {
-    key: 'cases',
-    label: 'Cases',
-    children:
-      <>
-        <Card
-          title="Endpoints do Looplex Cases"
-          actions={[]}
-        >
-          <a href="/v1/cases" target="_blank">Cases v1</a> <br />
-          <a href="/v1/case-management" target="_blank">Case Management</a>
-        </Card>
-      </>
-  },
-  {
-    key: 'looplex365',
-    label: 'Looplex 365',
-    children:
-      <>
-        <Card
-          title="Endpoints do Looplex 365"
-          actions={[]}
-        >
-          <a href="/v1/looplex365" target="_blank">Looplex 365 v1</a>
-        </Card>
-      </>
-  },
-  {
-    key: 'court_venue',
-    label: 'Court Venue',
-    children:
-      <>
-        <Card
-          title="Endpoints de Court Venue"
-          actions={[]}
-        >
-          <a href="/court_venue" target="_blank">Court Venue</a>
-        </Card>
-      </>
-  },
-  {
-    key: 'recess',
-    label: 'Feriados',
-    children:
-      <>
-        <Card
-          title="Endpoints de Feriados"
-          actions={[]}
-        >
-          <a href="/v1/recess" target="_blank">Feriados</a>
-        </Card>
-      </>
-  }
-]
-
 export default function Home() {
 
   return (
 
     <div style={{ backgroundColor: '#F5FBFE' }}>
 
-      <Row 
+      <Row
         style={{
           padding: 0,
           backgroundColor: '#F0F4F6',
@@ -125,9 +43,9 @@ export default function Home() {
         </Col>
 
         <Col span={10} style={{ marginTop: 30 }}>
-          <Search 
-            placeholder="Pesquise na Looplex OpenAPI" 
-            onSearch={() => {}}
+          <Search
+            placeholder="Pesquise na Looplex OpenAPI"
+            onSearch={() => { }}
             suffix={
               <AudioOutlined
                 style={{
@@ -149,25 +67,42 @@ export default function Home() {
 
       <Row>
 
-        <Col span={16}>
-          <Text style={{ fontSize: 32, fontWeight: 'bold', marginLeft: 100, marginTop: 50 }}>
-            Documentação - APIs Looplex
-          </Text>
+        <Col span={15} offset={1}>
+
+          <Row>
+
+            <Col span={16}>
+              <Text style={{ fontSize: 36, fontWeight: 'bold', marginTop: 50 }}>
+                Documentação - APIs Looplex
+              </Text>
+            </Col>
+
+          </Row>
+
+          <Row>
+
+            <Col span={16}>
+              <div style={{ fontSize: 18, marginTop: 30 }}>
+                Seja bem-vindo à página oficial de <b>documentação das APIs da Looplex.</b>
+              </div>
+              <br />
+              <div style={{ fontSize: 18 }}>
+                Aqui você encontrará informações detalhadas, guias e exemplos para auxiliar na
+                integração eficiente dos nossos serviços.
+              </div>
+            </Col>
+
+          </Row>
+
         </Col>
 
-      </Row>
-
-      <Row>
-      
-        <Col span={16}>
-          <div style={{ fontSize: 20, marginLeft: 100, marginTop: 30 }}>
-            Seja bem-vindo à página oficial de <b>documentação das APIs da Looplex.</b>
-          </div>
-          <br />
-          <div style={{ fontSize: 20, marginLeft: 100 }}>
-            Aqui você encontrará informações detalhadas, guias e exemplos para auxiliar na 
-            integração eficiente dos nossos serviços.
-          </div>
+        <Col span={7}>
+          <Image
+            src={'/api_image.png'}
+            width={500}
+            height={350}
+            alt='Looplex'
+          />
         </Col>
 
       </Row>
@@ -181,116 +116,92 @@ export default function Home() {
       <Row style={{ marginTop: 30, marginLeft: 100 }}>
 
         <Col span={7}>
-          
-          <Card style={{ height: 220 }}>
+
+          <Card style={{ height: 250 }}>
 
             <div>
               <ApiOutlined style={{ fontSize: 40, color: '#1677FF' }} />
-                <Text 
-                  style={{ 
-                    marginLeft: 15,
-                    fontSize: 22, 
-                    fontWeight: 'bold' 
-                  }}
-                >
-                  Actions
-                </Text>
+              <Text
+                style={{
+                  marginLeft: 15,
+                  fontSize: 22,
+                  fontWeight: 'bold'
+                }}
+              >
+                Actions
+              </Text>
             </div>
 
             <p>
               É o grupo de APIs usado pelos <b>templates</b> relacionados ao <b>fluxo de documentos.</b>
             </p>
 
-            <a 
-              href='/v1/actions' 
-              target='_blank'
-              style={{
-                backgroundColor: 'green',
-                height: 50
-              }}
-            >
-              <div style={{ textAlign: 'left' }}>
-                Actions v1
-              </div>
-            </a>
-              
+            <BorderedLink
+              text={'Actions v1'}
+              link={'/v1/actions'}
+            />
+
           </Card>
 
         </Col>
 
         <Col span={7} style={{ marginLeft: 20 }}>
 
-          <Card style={{ height: 220 }}>
+          <Card style={{ height: 250 }}>
 
-          <div>
+            <div>
               <ApiOutlined style={{ fontSize: 40, color: '#1677FF' }} />
-                <Text 
-                  style={{ 
-                    marginLeft: 15,
-                    fontSize: 22, 
-                    fontWeight: 'bold' 
-                  }}
-                >
-                  Court Venue
-                </Text>
+              <Text
+                style={{
+                  marginLeft: 15,
+                  fontSize: 22,
+                  fontWeight: 'bold'
+                }}
+              >
+                Court Venue
+              </Text>
             </div>
 
             <p>
-              É o grupo de APIs responsável por definir <b>a cidade ou 
-              tribunal</b> onde o <b>processo será julgado</b>, com base nos dados 
+              É o grupo de APIs responsável por definir <b>a cidade ou
+                tribunal</b> onde o <b>processo será julgado</b>, com base nos dados
               fornecidos.
             </p>
 
-            <a 
-              href='/court_venue' 
-              target='_blank'
-              style={{
-                backgroundColor: 'green',
-                height: 50
-              }}
-            >
-              <div style={{ textAlign: 'left' }}>
-                Court Venue
-              </div>
-            </a>
-              
+            <BorderedLink
+              text={'Court Venue'}
+              link={'/court_venue'}
+            />
+
           </Card>
 
         </Col>
 
         <Col span={7} style={{ marginLeft: 20 }}>
 
-          <Card style={{ height: 220 }}>
+          <Card style={{ height: 250 }}>
             <div>
-                <ApiOutlined style={{ fontSize: 40, color: '#1677FF' }} />
-                  <Text 
-                    style={{ 
-                      marginLeft: 15,
-                      fontSize: 22, 
-                      fontWeight: 'bold' 
-                    }}
-                  >
-                    Feriado
-                  </Text>
-              </div>
+              <ApiOutlined style={{ fontSize: 40, color: '#1677FF' }} />
+              <Text
+                style={{
+                  marginLeft: 15,
+                  fontSize: 22,
+                  fontWeight: 'bold'
+                }}
+              >
+                Feriado
+              </Text>
+            </div>
 
             <p>
               API de feriados é um serviço que fornece datas oficiais de feriados para diferentes regiões.
             </p>
 
-            <a 
-              href='/v1/recess' 
-              target='_blank'
-              style={{
-                backgroundColor: 'green',
-                height: 50
-              }}
-            >
-              <div style={{ textAlign: 'left' }}>
-                Feriado v1
-              </div>
-            </a>
-              
+            <BorderedLink
+              link='/v1/recess'
+              text={'Feriado v1'}
+            />
+
           </Card>
 
         </Col>
@@ -306,135 +217,123 @@ export default function Home() {
       <Row style={{ marginTop: 30, marginLeft: 100 }}>
 
         <Col span={7}>
-          
-          <Card style={{ height: 220 }}>
+
+          <Card style={{ height: 280 }}>
 
             <div>
               <ApiOutlined style={{ fontSize: 40, color: '#1677FF' }} />
-                <Text 
-                  style={{ 
-                    marginLeft: 15,
-                    fontSize: 22, 
-                    fontWeight: 'bold' 
-                  }}
-                >
-                  Cases
-                </Text>
+              <Text
+                style={{
+                  marginLeft: 15,
+                  fontSize: 22,
+                  fontWeight: 'bold'
+                }}
+              >
+                Cases
+              </Text>
             </div>
 
             <p>
-              API de casos gerencia dados e status de processos.<br/>
+              API de casos gerencia dados e status de processos.<br />
               Permite consultar e atualizar informações automaticamente.
             </p>
 
-            <a 
-              href='/v1/cases' 
-              target='_blank'
-              style={{
-                backgroundColor: 'green',
-                height: 50
-              }}
-            >
-              <div style={{ textAlign: 'left' }}>
-                Cases V1 (legado)
-              </div>
-            </a>
+            <BorderedLink
+              link='/v1/cases'
+              text={'Cases V1 (legado)'}
+            />
 
-            <a 
-              href='/v1/case-management' 
-              target='_blank'
-              style={{
-                marginTop: 15, 
-                width: '100%', 
-                height: 50, 
-                borderWidth: 10, 
-                borderColor: 'grey' 
-              }}
-            >
-              <div style={{ textAlign: 'left' }}>
-                Cases Management
-              </div>
-            </a>
-              
+            <BorderedLink
+              link='/v1/case-management'
+              text={'Cases Management'}
+            />
+
           </Card>
 
         </Col>
 
         <Col span={7} style={{ marginLeft: 20 }}>
-          
-          <Card style={{ height: 220 }}>
 
-          <div>
+          <Card style={{ height: 280 }}>
+
+            <div>
               <ApiOutlined style={{ fontSize: 40, color: '#1677FF' }} />
-                <Text 
-                  style={{ 
-                    marginLeft: 15,
-                    fontSize: 22, 
-                    fontWeight: 'bold' 
-                  }}
-                >
-                  Assembler
-                </Text>
+              <Text
+                style={{
+                  marginLeft: 15,
+                  fontSize: 22,
+                  fontWeight: 'bold'
+                }}
+              >
+                Assembler
+              </Text>
             </div>
             <p>
               É o grupo de APIs usado pela <b>interface web do Looplex</b>, relacionado ao <b>fluxo de documentos, templates e tarefas.</b>
             </p>
 
-            <a 
-              href='/v1/assembler' 
-              target='_blank'
-              style={{
-                backgroundColor: 'green',
-                height: 50
-              }}
-            >
-              <div style={{ textAlign: 'left' }}>
-                Assembler v1
-              </div>
-            </a>
-              
+            <BorderedLink
+              link='/v1/assembler'
+              text={'Assembler v1'}
+            />
+
           </Card>
 
         </Col>
 
         <Col span={7} style={{ marginLeft: 20 }}>
-          
-          <Card style={{ height: 220 }}>
 
-          <div>
+          <Card style={{ height: 280 }}>
+
+            <div>
               <ApiOutlined style={{ fontSize: 40, color: '#1677FF' }} />
-                <Text 
-                  style={{ 
-                    marginLeft: 15,
-                    fontSize: 22, 
-                    fontWeight: 'bold' 
-                  }}
-                >
-                  Looplex 365
-                </Text>
+              <Text
+                style={{
+                  marginLeft: 15,
+                  fontSize: 22,
+                  fontWeight: 'bold'
+                }}
+              >
+                Looplex 365
+              </Text>
             </div>
             <p>
               Ciclo de vida de aplicativos da microsoft e renderizações.
             </p>
 
-            <a 
-              href='/v1/looplex365' 
-              target='_blank'
-              style={{
-                backgroundColor: 'green',
-                height: 50
-              }}
-            >
-              <div style={{ textAlign: 'left' }}>
-                Looplex 365 v1
-              </div>
-            </a>
-              
+            <BorderedLink
+              link='/v1/looplex365'
+              text={'Looplex 365 v1'}
+            />
+
           </Card>
 
         </Col>
 
       </Row>
+    </div>
+  )
+}
+
+const BorderedLink = ({ text, link }) => {
+  return (
+    <div
+      style={{
+        border: "0.5px solid lightgrey",
+        borderRadius: 8,
+        padding: 10,
+        marginTop: 10
+      }}
+    >
+      <a
+        href={link}
+        target='_blank'
+      >
+        <div style={{ textAlign: 'left' }}>
+          {text}
+        </div>
+      </a>
+
     </div>
   )
 }
